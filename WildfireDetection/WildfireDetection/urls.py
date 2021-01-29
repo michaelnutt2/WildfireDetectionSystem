995cedfe06830324dbcec0b5d06befe660dc848d
michaelnutt <michaelnutt2@my.unt.edu>
2021-01-29 17:15:09 -0600

    Created admin site for testing

--- WildfireDetection/WildfireDetection/urls.py
+++ WildfireDetection/WildfireDetection/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wildfireapp/', include('wildfireDetectionApp.urls')),
+    path('', include('wildfireDetectionApp.urls')),
     path('admin/', admin.site.urls),
 ]
