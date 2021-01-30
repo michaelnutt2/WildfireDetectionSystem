d3000db71d931a2224e4ca86fe20f9850bbeba5d
michaelnutt <michaelnutt2@my.unt.edu>
2021-01-30 17:09:39 -0600

    Created demo index page for map to be filled in at

--- WildfireDetection/wildfireDetectionApp/views.py
+++ WildfireDetection/wildfireDetectionApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world')
+    template = loader.get_template('wildfireDetectionApp/index.html')
+    return render(request, 'wildfireDetectionApp/index.html')
+
